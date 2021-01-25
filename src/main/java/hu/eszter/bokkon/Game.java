c75aef1d246733112f0ad6317146101211c1c116
Eszter Bokkon <bokkone@gmail.com>
2021-01-25 13:25:08 +0100

    Instantiate a Game. Set up all participants.

--- src/main/java/hu/eszter/bokkon/Game.java
+++ src/main/java/hu/eszter/bokkon/Game.java
@@ -1,4 +1,22 @@
 package hu.eszter.bokkon;
 
+import hu.eszter.bokkon.model.participants.AnimalStock;
+import hu.eszter.bokkon.model.participants.Dice;
+import hu.eszter.bokkon.model.participants.Farmer;
+
 public class Game {
+
+    private AnimalStock animalStock;
+    private final Dice dice1;
+    private final Dice dice2;
+    private Farmer player1;
+    private Farmer player2;
+
+    public Game(AnimalStock animalStock, Dice dice1, Dice dice2, Farmer player1, Farmer player2) {
+        this.animalStock = animalStock;
+        this.dice1 = dice1;
+        this.dice2 = dice2;
+        this.player1 = player1;
+        this.player2 = player2;
+    }
 }
--- src/main/java/hu/eszter/bokkon/Main.java
+++ src/main/java/hu/eszter/bokkon/Main.java
@@ -1,8 +1,20 @@
 package hu.eszter.bokkon;
 
+import hu.eszter.bokkon.model.participants.AnimalStock;
+import hu.eszter.bokkon.model.participants.Dice;
+import hu.eszter.bokkon.model.participants.Farmer;
+import hu.eszter.bokkon.service.Initializer;
+
 public class Main {
 
     public static void main (String[] args) {
 
+        Initializer initializer = new Initializer();
+        AnimalStock animals = initializer.createAnimalStock();
+        Dice dice1 = initializer.createDice("sheep", "cow", "wolf");
+        Dice dice2 = initializer.createDice("pig", "horse", "fox");
+        Farmer farmer1 = new Farmer("Jen");
+        Farmer farmer2 = new Farmer("Bob");
+        Game newGame = new Game(animals, dice1, dice2, farmer1, farmer2);
     }
 }
