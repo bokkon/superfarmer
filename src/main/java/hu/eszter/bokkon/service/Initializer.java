29fe7838b5429c4a7268090ce2a4b8527f034f58
Eszter Bokkon <bokkone@gmail.com>
2021-01-25 15:32:49 +0100

    Correct createDice method in Initializer.

--- src/main/java/hu/eszter/bokkon/service/Initializer.java
+++ src/main/java/hu/eszter/bokkon/service/Initializer.java
@@ -5,10 +5,7 @@ import hu.eszter.bokkon.model.participants.AnimalStock;
 import hu.eszter.bokkon.model.participants.Dice;
 import hu.eszter.bokkon.model.participants.Farmer;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -32,9 +29,9 @@ public class Initializer {
     }
 
     public Dice createDice(String a1, String a2, String a3) {
-        List<String> diceSides = new ArrayList<>();
-        diceSides.addAll(Stream.of("rabbit").limit(6).collect(Collectors.toList()));
-        diceSides.addAll(Stream.of("sheep").limit(2).collect(Collectors.toList()));
+        List<String> diceSides = new ArrayList<>(Collections.nCopies(6, "rabbit"));
+        List<String> actList = Collections.nCopies(2, "sheep");
+        diceSides.addAll(actList);
         diceSides.add("pig");
         diceSides.addAll(Arrays.asList(a1, a2, a3));
         return new Dice(diceSides);
