486926968b1ce46e746460ea26166b171e4a080b
Eszter Bokkon <bokkone@gmail.com>
2021-01-26 10:34:49 +0100

    Delete unnecessary field in Cow.

--- src/main/java/hu/eszter/bokkon/model/animal/Cow.java
+++ src/main/java/hu/eszter/bokkon/model/animal/Cow.java
@@ -1,14 +1,4 @@
 package hu.eszter.bokkon.model.animal;
 
 public class Cow implements Animal {
-
-    private final String name;
-
-    public Cow() {
-        name = "cow";
-    }
-
-    public String getName() {
-        return name;
-    }
 }
