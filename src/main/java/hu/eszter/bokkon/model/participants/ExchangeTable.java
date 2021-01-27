27f41e1f455a31b36347cfcfab0f48c1f48095b2
Eszter Bokkon <bokkone@gmail.com>
2021-01-27 10:28:29 +0100

    Modify ExchangeTable. Comment code that may not be needed, will delete later.

--- src/main/java/hu/eszter/bokkon/model/participants/ExchangeTable.java
+++ src/main/java/hu/eszter/bokkon/model/participants/ExchangeTable.java
@@ -5,8 +5,6 @@ import hu.eszter.bokkon.model.animal.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ExchangeTable {
 
@@ -16,46 +14,44 @@ public class ExchangeTable {
 
     public ExchangeTable() {
         fillExchanges();
-        fillExchangesHelp1();
-        fillExchangesHelp2();
     }
 
     private void fillExchanges() {
-        exchanges.put("Sheep", "Rabbit6,SmallDog");
-        exchanges.put("Cow", "Pig3,BigDog");
-        exchanges.put("Pig", "Sheep2");
-        exchanges.put("Horse", "Cow2");
+        exchanges.put("Sheep", "SmallDog,6 Rabbit");
+        exchanges.put("Cow", "BigDog,3 Pig");
+        exchanges.put("Pig", "2 Sheep");
+        exchanges.put("Horse", "2 Cow");
         exchanges.put("SmallDog", "Sheep");
         exchanges.put("BigDog", "Cow");
 
-        exchanges.put("Rabbit6", "Sheep");
-        exchanges.put("Sheep2", "Pig");
-        exchanges.put("Pig3", "Cow");
-        exchanges.put("Cow2", "Horse");
+        exchanges.put("6 Rabbit", "Sheep");
+        exchanges.put("2 Sheep", "Pig");
+        exchanges.put("3 Pig", "Cow");
+        exchanges.put("2 Cow", "Horse");
     }
 
-    private void fillExchangesHelp1() {
-        List<Animal> act = Stream.generate(Rabbit::new).limit(6).collect(Collectors.toList());
-        exchangesHelp1.put("Rabbit6", act);
-        act.clear();
-        act = Stream.generate(Sheep::new).limit(2).collect(Collectors.toList());
-        exchangesHelp1.put("Sheep2", act);
-        act.clear();
-        act = Stream.generate(Pig::new).limit(3).collect(Collectors.toList());
-        exchangesHelp1.put("Pig3", act);
-        act.clear();
-        act = Stream.generate(Cow::new).limit(2).collect(Collectors.toList());
-        exchangesHelp1.put("Cow2", act);
-    }
-
-    private void fillExchangesHelp2() {
-        exchangesHelp2.put("Sheep", new Sheep());
-        exchangesHelp2.put("Pig", new Pig());
-        exchangesHelp2.put("Cow", new Cow());
-        exchangesHelp2.put("Horse", new Horse());
-        exchangesHelp2.put("SmallDog", new SmallDog());
-        exchangesHelp2.put("BigDog", new BigDog());
-    }
+//    private void fillExchangesHelp1() {
+//        List<Animal> act = Stream.generate(Rabbit::new).limit(6).collect(Collectors.toList());
+//        exchangesHelp1.put("Rabbit6", act);
+//        act.clear();
+//        act = Stream.generate(Sheep::new).limit(2).collect(Collectors.toList());
+//        exchangesHelp1.put("Sheep2", act);
+//        act.clear();
+//        act = Stream.generate(Pig::new).limit(3).collect(Collectors.toList());
+//        exchangesHelp1.put("Pig3", act);
+//        act.clear();
+//        act = Stream.generate(Cow::new).limit(2).collect(Collectors.toList());
+//        exchangesHelp1.put("Cow2", act);
+//    }
+//
+//    private void fillExchangesHelp2() {
+//        exchangesHelp2.put("Sheep", new Sheep());
+//        exchangesHelp2.put("Pig", new Pig());
+//        exchangesHelp2.put("Cow", new Cow());
+//        exchangesHelp2.put("Horse", new Horse());
+//        exchangesHelp2.put("SmallDog", new SmallDog());
+//        exchangesHelp2.put("BigDog", new BigDog());
+//    }
 
     public Map<String, String> getExchanges() {
         return exchanges;
