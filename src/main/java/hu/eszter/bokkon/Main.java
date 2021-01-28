304f1edb21331348ad739731549cb06fb740b768
Eszter Bokkon <bokkone@gmail.com>
2021-01-28 04:54:37 +0100

    Run methods from Main.

--- src/main/java/hu/eszter/bokkon/Main.java
+++ src/main/java/hu/eszter/bokkon/Main.java
@@ -6,5 +6,16 @@ public class Main {
 
         Game newGame = new Game();
         newGame.init();
+
+//        Farmer player = newGame.getFarmers().get(0);
+//        Map<Animal, Integer> stock = newGame.getAnimalStock().getLiveStock();
+//        Map<Animal, Integer> plStock = player.getFarmerLiveStock();
+//        plStock.put(new Cow(), 2);
+//        plStock.put(new Rabbit(), 3);
+//        plStock.put(new SmallDog(), 1);
+//        plStock.put(new Pig(), 2);
+//        plStock.put(new Sheep(), 1);
+//        Util util = new Util();
+//        util.printSimpleChangesMap(newGame.getPossibleSimpleChanges(player));
     }
 }
