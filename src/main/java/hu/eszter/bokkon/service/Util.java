3f42ceabaf3f2ae00169dbfda0da312f0a525d67
Eszter Bokkon <bokkone@gmail.com>
2021-02-02 13:11:31 +0100

    Refactor printSimpleChangesMap in Util according to getPossibleChanges method in Game, so that it can display the data correctly.

--- src/main/java/hu/eszter/bokkon/service/Util.java
+++ src/main/java/hu/eszter/bokkon/service/Util.java
@@ -7,16 +7,33 @@ import java.util.Map;
 
 public class Util {
 
-    public void printSimpleChangesMap(Map<Animal, Map<Animal, Integer>> map) {
-        for (Map.Entry<Animal, Map<Animal, Integer>> entry : map.entrySet()) {
+    /**
+     * Displays all the possible exchanges between 2 stocks: the actual farmer's animal stock and another animal stock.
+     *
+     * @param possibleChanges is a map data structure storing the possible exchanges between 2 animal stocks
+     */
+    public void printPossibleChangesMap(Map<Animal, Map<Animal, Double>> possibleChanges) {
+        int line = 1;
+        for (Map.Entry<Animal, Map<Animal, Double>> entry : possibleChanges.entrySet()) {
             String animalName = entry.getKey().getClass().getSimpleName();
-            Map<Animal, Integer> actMap = entry.getValue();
+            Map<Animal, Double> actMap = entry.getValue();
             for (Animal actAnimal : actMap.keySet()) {
-                int count = actMap.get(actAnimal);
-                System.out.println(animalName + String.join("", Collections.nCopies(8-animalName.length(), " "))
-                        + " for:   " + count + " " + actAnimal.getClass().getSimpleName() + (count == 1 ? "" : "s"));
+                System.out.print((line > 9 ? "" : " ") + line++ + ".) ");
+                double count = actMap.get(actAnimal);
+                if (count >= 1.0) {
+                    String spaces = getSpaces(8, animalName);
+                    System.out.println("1 " + animalName + spaces + " for:   "
+                            + (int) count + " " + actAnimal.getClass().getSimpleName() + (count == 1 ? "" : "s"));
+                } else if (count > 0.0) {
+                    String spaces = getSpaces(7, animalName);
+                    System.out.println((int)(1 / count) + " " + animalName + ((int)(1 / count) == 1 ? " " : "s") + spaces
+                            + " for:   1 " +  actAnimal.getClass().getSimpleName());
+                }
             }
         }
     }
 
+    private String getSpaces (int maxLength, String animalName) {
+        return String.join("", Collections.nCopies(maxLength - animalName.length(), " "));
+    }
 }
