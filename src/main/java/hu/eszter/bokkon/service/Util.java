381c282ea47286b05cd47df5d90bed073094bcd4
Eszter Bokkon <bokkone@gmail.com>
2021-02-03 10:34:12 +0100

    Refactor printPossibleChanges in Util.

--- src/main/java/hu/eszter/bokkon/service/Util.java
+++ src/main/java/hu/eszter/bokkon/service/Util.java
@@ -14,19 +14,18 @@ public class Util {
      */
     public static void printPossibleChangesMap(Map<Animal, Map<Animal, Double>> possibleChanges) {
         int line = 1;
-        for (Map.Entry<Animal, Map<Animal, Double>> entry : possibleChanges.entrySet()) {
-            String animalName = entry.getKey().getClass().getSimpleName();
-            Map<Animal, Double> actMap = entry.getValue();
+        for (Animal animal : possibleChanges.keySet()) {
+            String animalName = animal.getClass().getSimpleName();
+            Map<Animal, Double> actMap = possibleChanges.get(animal);
             for (Animal actAnimal : actMap.keySet()) {
                 System.out.print((line > 9 ? "" : " ") + line++ + ".) ");
                 double count = actMap.get(actAnimal);
                 if (count >= 1.0) {
-                    String spaces = getSpaces(8, animalName.length());
-                    System.out.println("1 " + animalName + spaces + " ===>   "
+                    System.out.println("1 " + animalName + getSpaces(8, animalName.length()) + " ===>   "
                             + (int) count + " " + actAnimal.getClass().getSimpleName() + (count == 1 ? "" : "s"));
                 } else if (count > 0.0) {
-                    String spaces = getSpaces(7, animalName.length());
-                    System.out.println((int) (1 / count) + " " + animalName + ((int) (1 / count) == 1 ? " " : "s") + spaces
+                    System.out.println((int) (1 / count) + " " + animalName + ((int) (1 / count) == 1 ? " " : "s")
+                            + getSpaces(7, animalName.length())
                             + " ===>   1 " + actAnimal.getClass().getSimpleName());
                 }
             }
