83d82b8e955fa69a735ce296107d2e497aa536ab
Eszter Bokkon <bokkone@gmail.com>
2021-02-04 12:09:48 +0100

    Call run method from Main.

--- src/main/java/hu/eszter/bokkon/Main.java
+++ src/main/java/hu/eszter/bokkon/Main.java
@@ -1,30 +1,24 @@
 package hu.eszter.bokkon;
 
 import hu.eszter.bokkon.model.animal.*;
-import hu.eszter.bokkon.model.participants.Farmer;
 import hu.eszter.bokkon.service.Util;
-
-import java.util.Map;
 
 public class Main {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         Game newGame = new Game();
+        Util.startMessage();
         newGame.init();
+        //TODO delete later, filling the farmers' stock with animals is for test purposes
+        newGame.getFarmers().get(0).addAnimals(new Rabbit(), 4);
+        newGame.getFarmers().get(0).addAnimals(new Horse(), 1);
+        newGame.getFarmers().get(0).addAnimals(new Sheep(), 10);
+        newGame.getFarmers().get(1).addAnimals(new Cow(), 6);
+        newGame.getFarmers().get(1).addAnimals(new Pig(), 3);
+        newGame.getFarmers().get(1).addAnimals(new SmallDog(), 1);
+//        Util.displayAllStocks(newGame.getAnimalBaseStock().getLiveStock(), newGame.getFarmers());
+        newGame.run();
 
-        //TODO delete this later
-        Farmer player = newGame.getFarmers().get(0);
-        Map<Animal, Integer> stock = newGame.getAnimalStock().getLiveStock();
-        Map<Animal, Integer> plStock = player.getFarmerLiveStock();
-        plStock.put(new Cow(), 2);
-        plStock.put(new Rabbit(), 3);
-        plStock.put(new SmallDog(), 1);
-        plStock.put(new Pig(), 2);
-        plStock.put(new Sheep(), 1);
-        Util util = new Util();
-        util.printSimpleChangesMap(newGame. getPossibleChanges(player));
-
-//        newGame.getDice1().getDiceSides().forEach( a -> System.out.println(a.getClass().getSimpleName()));
     }
 }
