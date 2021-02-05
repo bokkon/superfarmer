bd6db188927873f95bb1add6efce677d9bab1a00
Eszter Bokkon <bokkone@gmail.com>
2021-02-05 10:30:43 +0100

    Modify constructor of Dice, add IllegalArgumentException to ensure only to create 12 side dice.

--- src/main/java/hu/eszter/bokkon/model/participants/Dice.java
+++ src/main/java/hu/eszter/bokkon/model/participants/Dice.java
@@ -2,18 +2,18 @@ package hu.eszter.bokkon.model.participants;
 
 import hu.eszter.bokkon.model.animal.Animal;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Dice {
 
-    private final List<Animal> diceSides = new ArrayList<>(12);
+    private final List<Animal> diceSides;
 
     public Dice(List<Animal> diceSides) {
-        if (diceSides.size() == 12) {
-            this.diceSides.addAll(diceSides);
+        if (diceSides.size() != 12) {
+            throw new IllegalArgumentException("The dice must have 12 sides!");
         }
+        this.diceSides = diceSides;
     }
 
     public List<Animal> getDiceSides() {
