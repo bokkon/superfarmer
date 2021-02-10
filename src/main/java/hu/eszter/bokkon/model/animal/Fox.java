882f1da64be0c6c94ab4c3dfc118125abb02d9bd
Eszter Bokkon <bokkone@gmail.com>
2021-02-10 04:17:15 +0100

    Add overridden hashCode() and equals() methods to Fox and Wolf.

--- src/main/java/hu/eszter/bokkon/model/animal/Fox.java
+++ src/main/java/hu/eszter/bokkon/model/animal/Fox.java
@@ -1,17 +1,32 @@
 package hu.eszter.bokkon.model.animal;
 
-import java.util.HashMap;
 import java.util.Map;
 
-public final class Fox implements AttackerAnimal {
+public final class Fox implements Animal {
+
+    private final int id;
+
+    public Fox() {
+        this.id = 59;
+    }
 
     @Override
-    public void attack() {
+    public int hashCode() {
+        return id;
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+        Fox fox = (Fox) obj;
+        return (fox.id == this.id);
     }
 
     @Override
     public Map<Animal, Double> changeableTo() {
-        return new HashMap<>();
+        return null;
     }
 }
