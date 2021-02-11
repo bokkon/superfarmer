247cacb6b8de01c4e90a63cafced097188bc527c
Eszter Bokkon <bokkone@gmail.com>
2021-02-11 11:34:16 +0100

    Rename liveStock field to animalStock in AnimalBaseStock.

--- src/main/java/hu/eszter/bokkon/model/participants/AnimalBaseStock.java
+++ src/main/java/hu/eszter/bokkon/model/participants/AnimalBaseStock.java
@@ -8,26 +8,27 @@ import java.util.Map;
 /**
  * Base Stock providing all farm animals for 1 game.
  */
-public class AnimalBaseStock implements MoveAnimal {
+public class AnimalBaseStock implements StockOrganizer {
 
-    private Map<Animal, Integer> liveStock = new LinkedHashMap<>();
+    private Map<Animal, Integer> animalStock = new LinkedHashMap<>();
 
     public AnimalBaseStock() {
-        liveStock.put(new Rabbit(), 60);
-        liveStock.put(new Sheep(), 24);
-        liveStock.put(new Pig(), 20);
-        liveStock.put(new Cow(), 12);
-        liveStock.put(new Horse(), 6);
-        liveStock.put(new SmallDog(), 4);
-        liveStock.put(new BigDog(), 2);
+        animalStock.put(new Rabbit(), 60);
+        animalStock.put(new Sheep(), 24);
+        animalStock.put(new Pig(), 20);
+        animalStock.put(new Cow(), 12);
+        animalStock.put(new Horse(), 6);
+        animalStock.put(new SmallDog(), 4);
+        animalStock.put(new BigDog(), 2);
     }
 
-    public Map<Animal, Integer> getLiveStock() {
-        return liveStock;
+    @Override
+    public Map<Animal, Integer> getAnimalStock() {
+        return animalStock;
     }
 
     /**
-     * Implementation of addAnimals method of MoveAnimal interface. Adds given number of the same type of animal to
+     * Implementation of addAnimals method of StockOrganizer interface. Adds given number of the same type of animal to
      * the base animal stock, which is a map containing animal types as key and the number of that type of
      * animal as value, which is 0 by default.
      *
@@ -37,14 +38,14 @@ public class AnimalBaseStock implements MoveAnimal {
     @Override
     public void addAnimals(Animal animal, int howMany) {
         if (howMany >= 1) {
-            liveStock.computeIfPresent(animal, (k, v) -> v + howMany);
+            animalStock.computeIfPresent(animal, (k, v) -> v + howMany);
         } else {
             System.out.println("No animal was added to the base stock.");
         }
     }
 
     /**
-     * Implementation of removeAnimals method of MoveAnimal interface. Removes given number of the same type of animal
+     * Implementation of removeAnimals method of StockOrganizer interface. Removes given number of the same type of animal
      * from the base animal stock if there are available number of animals. In case there are no available animals from
      * 1 type, the animal type as key remains and its value becomes 0.
      *
@@ -53,8 +54,8 @@ public class AnimalBaseStock implements MoveAnimal {
      */
     @Override
     public void removeAnimals(Animal animal, int howMany) {
-        if (liveStock.get(animal) >= howMany) {
-            liveStock.computeIfPresent(animal, (k, v) -> v - howMany);
+        if (animalStock.get(animal) >= howMany) {
+            animalStock.computeIfPresent(animal, (k, v) -> v - howMany);
         } else {
             System.out.println("No animal was removed from the base stock.");
         }
@@ -62,7 +63,7 @@ public class AnimalBaseStock implements MoveAnimal {
     }
 
     public boolean isEmpty() {
-        for (int value: liveStock.values()) {
+        for (int value: animalStock.values()) {
             if (value != 0) {
                 return false;
             }
