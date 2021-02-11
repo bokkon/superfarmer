a9d3bb5bb71caee353d0b83c7f90f4d7b947b601
Eszter Bokkon <bokkone@gmail.com>
2021-02-11 11:16:58 +0100

    Change Integer to int in isEmpty method in AnimalBaseStock.

--- src/main/java/hu/eszter/bokkon/model/participants/AnimalBaseStock.java
+++ src/main/java/hu/eszter/bokkon/model/participants/AnimalBaseStock.java
@@ -62,7 +62,7 @@ public class AnimalBaseStock implements MoveAnimal {
     }
 
     public boolean isEmpty() {
-        for (Integer value: liveStock.values()) {
+        for (int value: liveStock.values()) {
             if (value != 0) {
                 return false;
             }
